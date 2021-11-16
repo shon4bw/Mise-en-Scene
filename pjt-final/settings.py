b23d7e604963ba52b242517f4b5278bf063a4754
CHOIYUNSEOK <choiys1995@gmail.com>
2021-11-16 17:49:15 +0900

    add swagger api

--- pjt-final/settings.py
+++ pjt-final/settings.py
@@ -34,12 +34,15 @@ INSTALLED_APPS = [
     'accounts',
     'community',
     'movies',
+
+    
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'drf_yasg',
 ]
 
 MIDDLEWARE = [
